05ae9b8c8e8a7c97543315d4f2147305c887de09
Andy Meagher <andy@chaninicholas.com>
2026-04-04 16:38:56 -0400

    adds capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.andy.lastreetlight",
+  appName: "remix-la-street-light",
+  webDir: "dist",
+};
+
+export default config;
