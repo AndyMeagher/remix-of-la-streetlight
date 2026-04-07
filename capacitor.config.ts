9de2f3d8e3d992f79a815ac3e25cce02b2cfbd2b
Andy Meagher <andy@chaninicholas.com>
2026-04-07 15:06:26 -0400

    updates Supabase project to personal

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.andy.lastreetlight",
-  appName: "remix-la-street-light",
+  appId: "com.lastreetlight.lastreetlight",
+  appName: "la-street-light",
   webDir: "dist",
 };
 
